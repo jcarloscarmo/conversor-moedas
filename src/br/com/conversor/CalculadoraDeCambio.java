502cd53a04e864f98e752e4dd7224f46b0a965c1
José Carlos <jcarllos.carmo@gmail.com>
2025-11-24 08:57:45 -0300

    add quantidade a converter

--- src/br/com/conversor/CalculadoraDeCambio.java
+++ src/br/com/conversor/CalculadoraDeCambio.java
@@ -1,5 +1,7 @@
 package br.com.conversor;
 
+import java.util.Scanner;
+
 public class CalculadoraDeCambio {
 
     public void calcular(String moedaOrigem, String moedaConvertida) {
@@ -8,12 +10,21 @@ public class CalculadoraDeCambio {
             Moeda novaMoeda = encontraTaxa.converterMoeda(moedaOrigem, moedaConvertida);
 
 
-            System.out.println("A taxa de conversão de 1 " + DicionarioDeMoedas.getNomeMoeda(moedaOrigem) +
-                    " para " + DicionarioDeMoedas.getNomeMoeda(moedaConvertida) +
-                    " é: " + novaMoeda.conversion_rate());
 
 
-            System.out.println("Retornando para o menu...\n\n");
+            System.out.println("\nDigite o valor que você deseja converter:");
+            Scanner leitura = new Scanner(System.in);
+            double valorParaConverter = leitura.nextDouble();
+            double valorConvertido = valorParaConverter * novaMoeda.conversion_rate();
+
+            System.out.printf("%.2f %s equivalem a %.2f %s",
+                    valorParaConverter,
+                    DicionarioDeMoedas.getNomeMoeda(moedaOrigem),
+                    valorConvertido,
+                    DicionarioDeMoedas.getNomeMoeda(moedaConvertida));
+
+
+            System.out.println("\nRetornando para o menu...\n\n");
             Thread.sleep(4000);
 
         } catch (RuntimeException | InterruptedException e) {
